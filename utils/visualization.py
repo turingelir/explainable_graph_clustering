3c0b7e975fd863a31da35e051b960567979f35bf
Fatih Said Duran <fatihdedi18@gmail.com>
2025-01-21 19:53:32 +0300

    Fixed show_mat

--- utils/visualization.py
+++ utils/visualization.py
@@ -6,15 +6,18 @@ from matplotlib import pyplot as plt
 def show_mat(mat, title: str, show: bool = False, return_fig: bool = False, save: bool = False, save_path: str = None,
              xlabel=None, ylabel=None, vmin=0.0, vmax=1.0, figsize=(10, 10), tight_layout=False,
              cmap: str = 'viridis', colorbar: bool = False):
-    plt.figure(figsize=figsize)
     if tight_layout:
-        plt.imshow(mat, cmap=cmap, vmin=vmin, vmax=vmax)
+        pos = plt.imshow(mat, cmap=cmap, vmin=vmin, vmax=vmax)
     else:
-        plt.matshow(mat, cmap=cmap, vmin=vmin, vmax=vmax)  # or plt.imshow ?
-    plt.title(title)     
+        pos = plt.matshow(mat, cmap=cmap, vmin=vmin, vmax=vmax)  # or plt.imshow ?  
+    # Set figsize
+    plt.figure(pos.get_figure().number, figsize=figsize)
+    plt.title(title)
+
     if colorbar:
-        pos = plt.pcolormesh(mat, cmap=cmap, vmin=vmin, vmax=vmax)
+        # pos = plt.pcolormesh(mat, cmap=cmap, vmin=vmin, vmax=vmax)
         plt.colorbar(pos, shrink=0.3).minorticks_on()
+        
     plt.axis('off')
     if tight_layout:
         plt.tight_layout()
@@ -28,3 +31,9 @@ def show_mat(mat, title: str, show: bool = False, return_fig: bool = False, save
         plt.close()
         plt.clf()
         return None
+
+if __name__ == '__main__':
+    # Test show_mat
+    import numpy as np
+    mat = np.random.rand(10, 10)
+    show_mat(mat, "Random Matrix", show=True, colorbar=True, tight_layout=True)
